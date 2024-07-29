ef1d710902b31396b73b2fcdfa7a46c67a74b677
isus203 <128573607+isus203@users.noreply.github.com>
2024-07-29 21:21:46 +0300

    Update and rename lineage_FULL-64-ARMV82.mk to lineage_KI7.mk

--- lineage_KI7.mk
+++ lineage_KI7.mk
@@ -11,17 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from FULL-64-ARMV82 device
-$(call inherit-product, device/tecno/FULL-64-ARMV82/device.mk)
+# Inherit from KI7 device
+$(call inherit-product, device/tecno/KI7/device.mk)
 
-PRODUCT_DEVICE := FULL-64-ARMV82
-PRODUCT_NAME := lineage_FULL-64-ARMV82
 PRODUCT_BRAND := TECNO
-PRODUCT_MANUFACTURER := tecno
+PRODUCT_DEVICE := KI7
+PRODUCT_NAME := lineage_KI7
+PRODUCT_MODEL := Tecno Spark 10 Pro
+PRODUCT_MANUFACTURER := Tecno Mobility Limited
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=TECNO-KI7 \
+    PRODUCT_NAME=KI7-GL \
     PRIVATE_BUILD_DESC="sys_tssi_64_armv82_tecno-user 13 TP1A.220624.014 354269 release-keys"
 
-BUILD_FINGERPRINT := TECNO/TSSI/FULL-64-ARMV82:13/TP1A.220624.014/230614V1762:user/release-keys
+BUILD_FINGERPRINT := TECNO/KI7-GL/TECNO-KI7:12/SP1A.210812.016/230615V2244:user/release-keys
